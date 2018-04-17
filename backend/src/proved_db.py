e1cf0fd84bb8d94a6391f71c500a963015c62643
Jay Pan <sfffaaa@gmail.com>
2018-04-17 22:25:26 +0200

    Add retrieve v1

--- backend/src/proved_db.py
+++ backend/src/proved_db.py
@@ -42,8 +42,11 @@ class ProvedJsonDB(ProvedBaseDB):
         with open(self._path, 'w') as f:
             json.dump(self._data, f)
 
-    def retrieve(self, myid):
-        pass
+    def retrieve(self, key):
+        if key not in self._data:
+            return ''
+        else:
+            return self._data[key]
 
     def update(self, entry):
         pass
@@ -67,10 +70,25 @@ class ProvedDB():
         self._type_db.create(entry)
         key, val = list(entry)[0], self._onchain_handler.hash_entry(entry)
         self._onchain_handler.create(key, val)
-        # use retrive to check it
 
-    def retrieve(self, myid):
-        pass
+        # use retrive to check it
+        retrieve_data = self.retrieve(key)
+        if retrieve_data != entry[list(entry)[0]]:
+            raise IOError('create fail...')
+
+    def retrieve(self, key):
+        db_data = self._type_db.retrieve(key)
+        onchain_exist, onchain_data = self._onchain_handler.retrieve(key)
+        if not onchain_exist:
+            if db_data or onchain_data:
+                raise IOError('key is not exist, shouldn\'t have any data, {0} v.s. {1}'.
+                              format(onchain_data, db_data))
+        else:
+            db_hash = self._onchain_handler.hash_entry({key: db_data})
+            if onchain_data != db_hash:
+                raise IOError('hash value doens\'t consist, {0} v.s. {1}'.
+                              format(onchain_data, db_hash))
+        return db_data
 
     def update(self, entry):
         pass
--- backend/testcase/proved_db_test.py
+++ backend/testcase/proved_db_test.py
@@ -51,7 +51,13 @@ class TestProvedDBJsonMethods(unittest.TestCase):
                 'mydata01': 'data01',
                 'mydata02': 'data02'
             }
-        }
+        },
+        'testRetrieveEntry': {
+            'test01': {
+                'mydata01': 'data01',
+                'mydata02': 'data02'
+            }
+        },
     }
 
     @classmethod
@@ -68,14 +74,7 @@ class TestProvedDBJsonMethods(unittest.TestCase):
     def tearDown(self):
         self.assertTrue(_unlink_silence(self._JSON_PATH))
 
-#    def testRetrieveNoEntry(self):
-#        onchain_handler = OnChainHandler(_TEST_CONFIG)
-#        exist, data = onchain_handler.retrieve('You should not exist!!!')
-#        self.assertEqual(exist, False, 'key not on chain')
-#        self.assertEqual(data, '', "data doesn't exist also!")
-
     def testCreateEntry(self):
-        self.assertTrue(_unlink_silence(self._JSON_PATH))
         testDB = ProvedDB(_TEST_CONFIG, 'json', self._JSON_PATH)
         data = self.TEST_DATA['testCreateEntry']
         testDB.create(data)
@@ -89,19 +88,38 @@ class TestProvedDBJsonMethods(unittest.TestCase):
         self.assertEqual(data, val, 'data on chain is inconsistent {0} != {1}'.format(data, val))
 
     def testCreateNoIdEntry(self):
-        self.assertTrue(_unlink_silence(self._JSON_PATH))
         testDB = ProvedDB(_TEST_CONFIG, 'json', self._JSON_PATH)
         data = self.TEST_DATA['testCreateNoIdEntry']
         self.assertRaisesRegex(IOError, 'input key should not more than one', testDB.create, data)
 
     def testCreateDuplicateID(self):
-        self.assertTrue(_unlink_silence(self._JSON_PATH))
         testDB = ProvedDB(_TEST_CONFIG, 'json', self._JSON_PATH)
         data = self.TEST_DATA['testCreateDuplicateID']
 
         testDB.create(data)
-        # [TODO] should check data inside
         self.assertRaisesRegex(IOError, 'unique key already exist', testDB.create, data)
+
+    def testRetrieveNoEntry(self):
+        onchain_handler = OnChainHandler(_TEST_CONFIG)
+        exist, data = onchain_handler.retrieve('You should not exist!!!')
+        self.assertEqual(exist, False, 'key not on chain')
+        self.assertEqual(data, '', "data doesn't exist also!")
+
+    def testRetrieveEntry(self):
+        testDB = ProvedDB(_TEST_CONFIG, 'json', self._JSON_PATH)
+        data = self.TEST_DATA['testRetrieveEntry']
+        testDB.create(data)
+
+        retrieve_data = testDB.retrieve(list(data)[0])
+        check_data = data[list(data)[0]]
+        self.assertEqual(retrieve_data, check_data,
+                         'retrive should be the same data, {0} != {1}'.format(retrieve_data, check_data))
+
+        onchain_handler = OnChainHandler(_TEST_CONFIG)
+        key, val = list(data)[0], onchain_handler.hash_entry(data)
+        exist, data = onchain_handler.retrieve(key)
+        self.assertEqual(exist, True, 'key is not on chain')
+        self.assertEqual(data, val, 'data on chain is inconsistent {0} != {1}'.format(data, val))
 
 #     def testUpdateEntry(self):
 #         self.assertTrue(False)
